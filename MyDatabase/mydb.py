095f42d0f00605a9e67fda8bfbf94a535222e19e
Rock's Vishu <you@example.com>
2019-01-18 11:46:11 +0530

    Adding more functions to database

--- MyDatabase/mydb.py
+++ MyDatabase/mydb.py
@@ -2,31 +2,28 @@ import json
 import os
 import random
 
-def load(filename='hello.db'):
-    MyDb = MyDatabase(filename)
+def load(filename='hello.db',autodump=False):
+    MyDb = MyDatabase(filename,autodump)
     MyDb.load_file()
     return MyDb
 
 class MyDatabase():
-    fileName = ''
     json_object = {}
-    def __init__(self,fileName):
+    def __init__(self,fileName,autodump):
         self.fileName = fileName
+        self.autodump = autodump
 
     def load_file(self): # Here the self is object or instance 
+        print("Loading database from",self.fileName)
         if os.path.exists(self.fileName):
-            print("Loading database from",self.fileName)
             content = open(self.fileName).read()
             self.json_object = json.loads(content)
-            print("Databse Loaded sucessfully")
         else:
-            print("Loading database from",self.fileName)
             with open(self.fileName,'w') as data:
                 json.dump(self.json_object,data)
-
             content = open(self.fileName).read()
             self.json_object = json.loads(content)
-            print("Databse Loaded sucessfully")
+        print("Databse Loaded sucessfully")
         return self.json_object
 
     def dump(self):
@@ -43,11 +40,12 @@ class MyDatabase():
             dic = self.load_file()
             return dic[key]
         except KeyError:
-             print("Enter correct Key to access the values")
+             return("Enter correct Key to access the values")
 
     def set(self,key,value):
         dic = self.load_file()
         dic[key] = value
+        self.auto_dump()
         return dic
 
     def get_all(self):
@@ -85,7 +83,20 @@ class MyDatabase():
     def random_insert(self,number):
         dic = self.load_file()
         for i in range(number):
-            random_keys = random.randint(1,10000)
-            random_values = random.randint(1,10000)   
+            random_keys = random.randint(0,10000)
+            random_values = random.randint(0,10000)   
             dic[random_keys] = random_values
+        self.auto_dump()
         return dic
+
+    def auto_dump(self):
+        if self.autodump:
+            self.dump()
+
+    def dmerge(self,otherDb):
+        dic1 = self.load_file()
+        new_object = MyDatabase(otherDb,self.autodump)
+        dic2 = new_object.load_file()
+        dic1.update(dic2)
+        self.auto_dump()
+        return "Your data merge sucessfully to "+ self.fileName
--- MyDatabase/useddb.py
+++ MyDatabase/useddb.py
@@ -1,14 +1,19 @@
 import mydb
-db = mydb.load('table.db')
-db.set('sunil','vandu')
-db.dump()
-b = db.exists('sunil')
-print(b)
-c = db.total_keys()
-print(c)
-d = db.del_db()
-print(d)
 
-e = db.random_insert(10)
-print(e)
+db1 = mydb.load('data/student1.db',True)
+db2 = mydb.load('data/extradata.db',True)
 
+db1.set('firstName','Vishal')
+db1.set('lastName','Gaddam')
+db1.set('contact',5100220113)
+db1.set('place','Mumbai')
+
+
+db2.set('age',21)
+db2.set('gender','Male')
+
+a = db1.dmerge('data/extradata.db')
+print(a)
+
+# db3 = mydb.load('data/student1.db')
+# db3.get('place')
